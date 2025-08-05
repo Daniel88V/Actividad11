5cf8e4f5d91c70b519e34ce9447d7987d451a45e
Daniel88V <daniel12velo@gmail.com>
2025-08-04 19:32:27 -0600

    Actividad11

--- quick_sort.py
+++ quick_sort.py
@@ -1,11 +1,17 @@
+def quick_sort(lista):
+    if len(lista) <= 1:
+        return lista
+    pivote = lista[0]
+    menores = [x for x in lista[1:] if x < pivote]
+    iguales = [x for x in lista if x == pivote]
+    mayores = [x for x in lista[1:] if x > pivote]
+    return quick_sort(menores) + iguales + quick_sort(mayores)
 print("¿Cuántos nombres desea ingresar?: ")
 cantidad = int(input())
+estudiantes = []
 for i in range (cantidad):
-    def quick_sort(lista):
-        if len(lista) <= 1:
-            return lista
-        pivote = lista[0]
-        menores = [x for x in lista[1:] if x < pivote]
-        iguales = [x for x in lista if x == pivote]
-        mayores = [x for x in lista[1:] if x > pivote]
-        return quick_sort(menores) + iguales + quick_sort(mayores)
+    print(f"Estudiante #{i+1}: ")
+    nombre = input("Ingrese el nombre del estudiante: ")
+    estudiantes.append(nombre)
+resultado = quick_sort(estudiantes)
+print(resultado)
